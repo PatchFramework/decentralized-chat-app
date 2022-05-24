dca4112df56ce2a4d23b8ec7c4caf97e1c07ee3c
Dominic Viola <60390517+PatchFramework@users.noreply.github.com>
2022-05-24 16:48:54 +0200

    Fix: Update DB content on frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Gun from 'gun'
-import {useState, useEffect} from 'react' 
+import { useState, useEffect } from 'react'
 import MessageForm from './MessageForm/MessageForm';
 
 const gun = Gun({
@@ -10,37 +10,37 @@ const gun = Gun({
 function App() {
   const [message, setMessage] = useState() // update the webpage if this text changes
   const [dbMessages, setDbMessages] = useState()
-  
-  // setDbMessages(<li>Empty</li>)
-  
-  const getMessagesFromDb = (dbItem) => {
-      let out = []
-      let list = gun.get(dbItem)
-      if (list) {
-          //loop through the entries in the database list
-          list.map().on((item, id) => {
-              // add from db to vs-variable if not empty
-              if (item) {
-                  out.push(item)
-                  console.log(id)
-              }
-          })
-      }
-      return out
-  }
+  useEffect(() => {
+    setDbMessages(getMessagesFromDb("messages"))
 
-  let dbitems = getMessagesFromDb("messages")
+  }, [])
+  const getMessagesFromDb = (dbItem) => {
+    let out = []
+    let list = gun.get(dbItem)
+    if (list) {
+      console.log(list)
+      //loop through the entries in the database list
+      list.map().on((item, id) => {
+        // add from db to vs-variable if not empty
+        out.push(item)
+        console.log(id)
+      })
+    }
+    return out
+  }
 
   return (
     <div className="App">
       <div>Last sent message: {message}</div>
-      <MessageForm setMessage={setMessage} gun={gun}/>
+      <MessageForm setMessage={setMessage} gun={gun} />
       <div>Database content:</div>
-            <ul>
-                {dbitems.map((v, k) => {
-                  return <li key={k}>{v}</li>
-                })}
-            </ul>
+      <ul>
+        {console.log(dbMessages)}
+        {dbMessages && dbMessages.map((v, k) => {
+          console.log(`database key: ${k} value: ${v}`)
+          return <li key={k}>{v}</li>
+        })}
+      </ul>
     </div>
   );
 }
