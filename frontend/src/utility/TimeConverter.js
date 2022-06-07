bfa5db80c1d70685d8877b82559100e3897f77f8
Dominic Viola <60390517+PatchFramework@users.noreply.github.com>
2022-06-07 18:13:37 +0200

    Fix: Time with 0 padding

--- frontend/src/utility/TimeConverter.js
+++ frontend/src/utility/TimeConverter.js
@@ -1,20 +1,23 @@
+const addZeroPaddingIfNecessary = (value) => {
+  //if input has one digit pad with one 0 on the left to get to two digits
+  //e.g. 5 -> "05"
+    let v = value.toString()
+    if (v.length === 1) v = "0" + v
+    return v
+}
+
 function TimeConverter(timestamp) {
   // convert the timestamp to a date in dd.mm.yy format with 0 padding
     let t = new Date(timestamp)
     console.log("Timestamp is:", t)
 
-    let hours = t.getHours().toString()
+    let hours = addZeroPaddingIfNecessary(t.getHours())
     
-    let minutes = t.getMinutes().toString()
+    let minutes = addZeroPaddingIfNecessary(t.getMinutes())
     
-    let day = t.getDate().toString()
-    //if month has one digit pad with 0 for two digits
-    if (day.length === 1) day = "0" + day
-    
-    let month = t.getMonth()+1 // convert (0-11) to (1-12)
-    month = month.toString()
-    //if month has one digit pad with 0 for two digits
-    if (month.length === 1) month = "0" + month
+    let day = addZeroPaddingIfNecessary(t.getDate())
+
+    let month = addZeroPaddingIfNecessary(t.getMonth()+1) // +1 will convert (0-11) to (1-12)
     
     let year = t.getFullYear().toString().slice(2, 4) // only use the last two digits of year
   
