7f951a245f285f4e3d00259177e4dfe8bf0e73dc
Dominic Viola <60390517+PatchFramework@users.noreply.github.com>
2022-06-07 17:59:35 +0200

    Add: Alert for illegal username and message input

--- frontend/src/ChatUsername/ChatUsername.js
+++ frontend/src/ChatUsername/ChatUsername.js
@@ -1,8 +1,11 @@
 
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import "./ChatUsername.css"
 import Button from 'react-bootstrap/Button'
+import Alert from 'react-bootstrap/Alert'
+
+const illegalValues = ["", undefined]
 
 function ChatroomUsername(props) {
     // props
@@ -18,17 +21,32 @@ function ChatroomUsername(props) {
     useEffect(() => {
         if(inputRef.current) inputRef.current.focus(); 
     }, [inputRef]);
-        
+    
+    // store state of illegal input alert in state and in localStorage 
+    const [alert, setAlert] = useState(JSON.parse(window.localStorage.getItem("wrongUsername")) || false) //show or hide bad input alert
+
+    useEffect(() => {
+        console.log("change alert to:", alert);
+        window.localStorage.setItem("wrongUsername", JSON.stringify(alert))
+    }, [alert])
+    
     // handle the value of the username input field
     const handleChange = event => {
         tempuser = event.target.value
+        // hide alert if user changes username
+        if (!(illegalValues.includes(tempuser))) setAlert(false)
         //userlist(tempuser);
         console.log('Current Username is:', tempuser);
     };
     const handleClick = () => {
-        setUsername(tempuser)
-        console.log('Username is', tempuser);
-        navigate("/home");
+        console.log("handle click with", alert);
+        // show alert if input is not allowed redirect to home page otherwise
+        if (illegalValues.includes(tempuser)) setAlert(true)
+        else {
+            setUsername(tempuser)
+            console.log('Username is', tempuser)
+            navigate("/home")
+        }
     };
     
 
@@ -37,6 +55,7 @@ function ChatroomUsername(props) {
         <h2 class> Choose a username </h2>
         <form onSubmit={handleClick}>
             <div class="user-box">
+                {alert && <Alert variant="danger" onClose={() => setAlert(false)} dismissible>This username is not allowed.</Alert>}
                 <input type="text" id="username" name="username" onChange={handleChange} ref={inputRef}/>
                 <Button variant="success" type='submit'>Submit</Button>
             </div>
--- frontend/src/MessageForm/MessageForm.js
+++ frontend/src/MessageForm/MessageForm.js
@@ -1,41 +1,50 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import Alert from 'react-bootstrap/Alert'
 import Form from 'react-bootstrap/Form'
 import InputGroup from 'react-bootstrap/InputGroup'
 import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
 import { useEffect, useRef } from 'react'
 
+const illegalValues = [undefined, ""]
+
 function MessageForm(props) {
+    var v = "" //tracks the user input
+    
+    // store state of illegal input alert in state and in localStorage 
+    const [alert, setAlert] = useState(window.localStorage.getItem("wrongMessageAlert") || false) //show or hide bad input alert
+    useEffect(() => {
+        window.localStorage.setItem("wrongMessageAlert", alert)
+    }, [alert])
+
     //keep focus on the message input field on every rerender
     const inputRef = useRef();
     useEffect(() => {
         if(inputRef.current) inputRef.current.focus(); 
        }, [inputRef]);
 
-    var v = "" //tracks the user input
-
     const addMessage = () => {
         console.log("Adding message", v, " from ", props.user, " to chatroom ", props.gunRoom);
-        if (v !== "" && v !== undefined) {
-        // read the text from the input box, if the button is clicked
-        console.log(`Add the message: ${v}`)
-        let dbMessages = props.gunRoom
-
-        // .set will append the message to a list; .put would replace it
-        dbMessages.set({
-            sender: props.user,
-            time: Date.now(),
-            data: v
-        })
-        
-        props.setMessage(v) //set the message state and rerender the page
+        if (!(illegalValues.includes(v))) {
+            // read the text from the input box, if the button is clicked
+            let dbMessages = props.gunRoom
+            
+            // save the new message to the database
+            // .set will append the message to a list; .put would replace it
+            dbMessages.set({
+                sender: props.user,
+                time: Date.now(),
+                data: v
+            })
+        } else {
+            setAlert(true)
         }
     }
 
     return (
         <div>
             <Form onSubmit={addMessage}>
+            {alert && <Alert variant="danger" onClose={() => setAlert(false)} dismissible>Your input is not allowed.</Alert>}
                 <InputGroup className="mb-3">
                     <FormControl
                         placeholder="Message"
